4afff2353b8d1a5ca3f7edc402112cedba8ed17c
ArcherCheng <archer.cheng@msa.hinet.net>
2024-09-20 08:24:18 +0800

    add errorException

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-test-errors',
@@ -8,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrl: './test-errors.component.css'
 })
 export class TestErrorsComponent {
+  baseUrl = 'https://localhost:5001/api/';
+  http = inject(HttpClient);
+  validationErrors: string[]=[];
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'buggy/register').subscribe({
+      next: (reponse) => { console.log(reponse); },
+      error: (error) => { 
+        console.log(error);
+        this.validationErrors = error;
+      },
+      complete: () => { console.log("request hsa completed") }
+    })
+  }
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: (reponse) => { console.log(reponse); },
+      error: (error) => { console.log(error); },
+      complete: () => { console.log("request hsa completed") }
+    });
+  }
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: (reponse) => { console.log(reponse); },
+      error: (error) => { console.log(error); },
+      complete: () => { console.log("request hsa completed") }  
+    })
+  }
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: (reponse) => { console.log(reponse); },
+      error: (error) => { console.log(error); },
+      complete: () => { console.log("request hsa completed") }
+    });
+  }
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: (reponse) => { console.log(reponse); },
+      error: (error) => { this.validationErrors = error.errors },
+      complete: () => { console.log("request hsa completed") }
+    });
+  }
 
 }
